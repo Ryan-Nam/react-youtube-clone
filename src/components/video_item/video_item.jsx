c9bbc2b06b86c8866deb3ccaa1c1615978fea472
Ryan-Nam <alfofh7060@gmail.com>
2021-10-01 15:48:00 +1000

    Add Css

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from './video_item.module.css';
+// Deconstructing
+const Video_item = ({video : {snippet}}) => (
+<li className={styles.constainer}>
+    <div className={styles.video}>
+        <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} art="video thumbail"></img>
+    {/* meta data */}
+    <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+    </div>
+    </div>
+</li>
 
-const Video_item = (props) => 
-<li>
-    <img src={props.video.snippet.thumbnails.medium.url} art="video thumbail"></img>
-
-</li>;
+);
 
 export default Video_item;
